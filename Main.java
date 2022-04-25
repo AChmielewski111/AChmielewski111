69c2c58b2d18ce9b02106d10cd028861b575fa3c
AChmielewski111 <51801651+AChmielewski111@users.noreply.github.com>
2022-04-25 18:25:20 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    Human czlowiek = new Human();
+    Goldfish ryba = new Goldfish();
+    System.out.println(czlowiek);
+    System.out.println(ryba);
+    }
+}
